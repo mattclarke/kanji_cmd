4231078fe3493e5f9b45c04cbd7375b78df13188
Matt Clarke <matt.clarke@esss.se>
2022-08-29 11:34:22 +0200

    Added another kanji.

--- kanji_cmd/lessons/lesson_1.py
+++ kanji_cmd/lessons/lesson_1.py
@@ -10,7 +10,14 @@ cards = {
         "右",
         "RIGHT",
         "ユウ, みぎ",
-        "[In shock over the illegal U-turn (ユウ)], he covered his mouth (口) with his hand from the RIGHT side.",
+        "[In shock over the illegal U-turn (ユウ)], he covered his 'mouth' with his hand from the RIGHT 'side'.",
         False,
+    ),
+    3: (
+        "雨",
+        "RAIN",
+        "ウ, あめ",
+        "[If you take the U-Boot (ウ) to AMErika (あめ), then you won’t notice the RAIN]. They just know it’s RAINING when the 'one' 'towel' they hang out is dripping wet.",
+        True,
     ),
 }
