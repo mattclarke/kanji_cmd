a481c4ed6bf34846179863f1c39664fed0c66673
Matt Clarke <matt.clarke@esss.se>
2022-09-19 15:18:43 +0200

    Improved documentation.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kanji_cmd
 Command line based kanji tester
+
+## Deploying and running
+
+- run `rm -rf build dist && python setup.py sdist bdist_wheel`
+- `pip install dist/kanji_cmd-0.0.1-py3-none-any.whl`
+- ``
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="kanji_cmd",
-    version="0.1.0",
+    version="0.0.1",
     description="Command line based kanji tester",
     long_description="Command line based kanji tester",
     url="https://github.com/mattclarke/kanji_cmd",
